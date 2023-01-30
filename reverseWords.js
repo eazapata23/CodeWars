9947bf8f5dea0b008ee50f178d6563d0740fc583
eazapata23 <eazapata23@gmail.com>
2023-01-30 17:46:56 -0500

    9th commit

---
+++ reverseWords.js
@@ -0,0 +1,12 @@
+// Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+
+// Examples
+// "This is an example!" ==> "sihT si na !elpmaxe"
+// "double  spaces"      ==> "elbuod  secaps"
+
+
+function reverseWords(str) {
+    let words = str.match(/[\S]+|\s+/g);
+    let reversedWords = words.map(word => word.split('').reverse().join(''));
+    return reversedWords.join('');
+}
