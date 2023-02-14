3e2b8cb657bc8a4716007ef0eee9b042a07a9a6b
eazapata23 <eazapata23@gmail.com>
2023-02-14 11:59:26 -0500

    isPalindrome solution 2

--- isPalindrome.js
+++ isPalindrome.js
@@ -19,3 +19,16 @@ const isPalindrome = word => word === word.split('').reverse().join('');
 console.log(isPalindrome('racecar'), 'true');
 console.log(isPalindrome('bob'), 'true');
 console.log(isPalindrome('tacos'), 'false');
+
+var isPalindrome2 = function (x) {
+    if (x < 0) return false;
+    if (x < 10) return true;
+    if (x % 10 === 0) return false;
+
+    let reversed = 0;
+    while (x > reversed) {
+        reversed = reversed * 10 + x % 10;
+        x = Math.floor(x / 10);
+    }
+    return x === reversed || x === Math.floor(reversed / 10);
+};
